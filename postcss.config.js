893f9d0fcaa41c65471609b979fb7f4c17e93751
nerrons <coniinedieu@gmail.com>
2019-12-16 15:11:18 +0800

    set up stylelint & postcss

---
+++ postcss.config.js
@@ -0,0 +1,21 @@
+import purge from '@fullhuman/postcss-purgecss'
+const purgecss = purge({
+  // Specify the paths to all of the template files in your project
+  content: [
+    './src/**/*.html',
+    './src/**/*.vue',
+    './src/**/*.jsx'
+    // etc.
+  ],
+
+  // Include any special characters you're using in this regular expression
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+})
+
+module.exports = {
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+    ...(process.env.NODE_ENV === 'production' ? [purgecss] : [])
+  ]
+}
