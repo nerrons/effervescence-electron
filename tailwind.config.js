2f66f0596594bf85c3d2f1062a94d25f08e9467c
nerrons <coniinedieu@gmail.com>
2019-12-16 16:33:22 +0800

    add font

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  theme: {
+    fontFamily: {
+      iosevka: [
+        '"Iosevka Term Slab Web"',
+        '-apple-system',
+        'BlinkMacSystemFont',
+        '"Segoe UI"',
+        'Roboto',
+        '"Helvetica Neue"',
+        'Arial',
+        '"Noto Sans"',
+        'sans-serif',
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"',
+        '"Noto Color Emoji"'
+      ]
+    },
+    extend: {}
+  },
+  variants: {},
+  plugins: []
+}
